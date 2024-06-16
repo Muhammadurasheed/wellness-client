31464b507f9c3d6051a62806f86f13220bfd04b8
Amos omohodion <amosomohodion@gmail.com>
2024-06-16 17:16:35 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          this is my react app
+          Learn React
         </a>
       </header>
     </div>
